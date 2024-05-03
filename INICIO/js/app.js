ee5715e6575f4db8ab7f74e3cddcd184b676537d
danielquispl <148181131+danielquispl@users.noreply.github.com>
2024-05-03 01:13:26 -0500

    add: hoy 03 / 05

--- INICIO/js/app.js
+++ INICIO/js/app.js
@@ -1,4 +1,24 @@
 const nav =document.querySelector('.secciones');
     window.addEventListener('scroll', function(){
         nav.classList.toggle('active', this.window.scrollY >0)
-    })
+    });
+
+var logo;
+function loadNow(opacity){
+    if(opacity <= 0){
+        displayContent();
+    }else{
+        logo.style.opacity=opacity;
+        window.setTimeout(function(){
+            loadNow(opacity -0.1)
+        }, 50);
+    }
+}
+function displayContent(){
+    logo.style.display='none';
+    document.getElementById('general').style.display='block';
+} 
+    document.addEventListener("DOMContentLoaded", function(){
+    logo = document.getElementById('logo');
+    loadNow(2);
+});
